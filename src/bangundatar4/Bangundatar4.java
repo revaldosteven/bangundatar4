400b69ea25ccc3382bb960ac42c35a59d7d76bce
Good_Poeple <Good_Poeple@DESKTOP-N0QA212>
2024-09-17 14:01:38 +0700

    first

---
+++ src/bangundatar4/Bangundatar4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bangundatar4;
+
+/**
+ *
+ * @author Good_Poeple
+ */
+public class Bangundatar4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
